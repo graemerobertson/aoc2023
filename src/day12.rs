0e145993f8ea4fc70e8a03025acefd155f857e93
Graeme Robertson <grobertson@microsoft.com>
2023-12-12 09:27:19 +0000

    Break the back of day 12

--- src/day12.rs
+++ src/day12.rs
@@ -1,8 +1,24 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
+fn count_possible_arrangements(row: &[char], arrangement: &[u32]) -> u64 {
+    let mut count: u64 = 1;
+    count
+}
+
 pub(crate) fn day12() {
     let f: File = File::open("data/day12.txt").unwrap();
     let reader: BufReader<File> = BufReader::new(f);
-    let _lines = reader.lines().collect::<io::Result<Vec<String>>>().unwrap();
+    let lines = reader.lines().collect::<io::Result<Vec<String>>>().unwrap();
+    let mut sum: u64 = 0;
+    for line in lines {
+        let split = line.split_whitespace().collect::<Vec<&str>>();
+        let row = split[0].chars().collect::<Vec<char>>();
+        let arrangement = split[1]
+            .split(',')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>();
+        sum += count_possible_arrangements(&row, &arrangement);
+    }
+    println!("Day 12 part 1: {}", sum);
 }
