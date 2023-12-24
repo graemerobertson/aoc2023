1620518f4819d62040a00f15a11f31e9797904ee
Graeme Robertson <grobertson@microsoft.com>
2023-12-24 17:49:22 +0000

    Day 23

--- src/day23.rs
+++ src/day23.rs
@@ -8,6 +8,74 @@ enum TileType {
     IcySlope((isize, isize)),
 }
 
+// Map of junctions to their adjacent junctions plus distance.
+type Nodes = HashMap<Junction, Vec<(Junction, usize)>>;
+type Junction = (isize, isize);
+
+// This function builds up the Nodes map by recursively traversing the path, but it's a bit of a
+// mess and I can't be bothered to tidy it up.
+fn build_junction_graph(
+    start: (isize, isize),
+    first_step: (isize, isize),
+    path: &HashMap<(isize, isize), TileType>,
+    junction_graph: &mut Nodes,
+) {
+    let mut path_to_next_junction: HashSet<(isize, isize)> = HashSet::new();
+    path_to_next_junction.insert(start);
+    path_to_next_junction.insert(first_step);
+    let mut next_step = first_step;
+    let mut distance = 0;
+    loop {
+        distance += 1;
+        let mut neighbours: Vec<(isize, isize)> = Vec::new();
+        for delta in [(0, 1), (1, 0), (-1, 0), (0, -1)] {
+            if path.contains_key(&(next_step.0 + delta.0, next_step.1 + delta.1))
+                && !path_to_next_junction.contains(&(next_step.0 + delta.0, next_step.1 + delta.1))
+            {
+                neighbours.push((next_step.0 + delta.0, next_step.1 + delta.1));
+            }
+        }
+        if neighbours.is_empty() {
+            let x = if next_step.0 == 1 { 0 } else { next_step.0 + 1 };
+            if let std::collections::hash_map::Entry::Vacant(e) = junction_graph.entry(start) {
+                e.insert(vec![((x, next_step.1), distance + 1)]);
+            } else {
+                junction_graph
+                    .get_mut(&start)
+                    .unwrap()
+                    .push(((x, next_step.1), distance + 1));
+            }
+            break;
+        }
+        if neighbours.len() == 1 {
+            path_to_next_junction.insert(neighbours[0]);
+            next_step = neighbours[0];
+        } else {
+            if let std::collections::hash_map::Entry::Vacant(e) = junction_graph.entry(start) {
+                e.insert(vec![(next_step, distance)]);
+            } else {
+                junction_graph
+                    .get_mut(&start)
+                    .unwrap()
+                    .push((next_step, distance));
+            }
+            if !junction_graph.contains_key(&next_step) {
+                for delta in [(0, 1), (1, 0), (-1, 0), (0, -1)] {
+                    if path.contains_key(&(next_step.0 + delta.0, next_step.1 + delta.1)) {
+                        build_junction_graph(
+                            next_step,
+                            (next_step.0 + delta.0, next_step.1 + delta.1),
+                            path,
+                            junction_graph,
+                        );
+                    }
+                }
+            }
+            break;
+        }
+    }
+}
+
 fn count_max_steps_direction(
     start: (isize, isize),
     end: (isize, isize),
@@ -42,34 +110,6 @@ fn count_max_steps_direction(
     count
 }
 
-fn count_max_steps_direction_part2(
-    start: (isize, isize),
-    end: (isize, isize),
-    next_step: (isize, isize),
-    path: &HashMap<(isize, isize), TileType>,
-    current_count: &mut usize,
-    best: &mut usize,
-) -> Option<isize> {
-    let next = (start.0 + next_step.0, start.1 + next_step.1);
-    if next == end {
-        *current_count += 1;
-        if current_count > best {
-            *best = *current_count;
-        }
-        return Some(1);
-    }
-    if path.contains_key(&next) {
-        *current_count += 1;
-        let mut remaining_path = path.clone();
-        remaining_path.remove(&next);
-        if let Some(count) = count_max_steps_part2(next, end, &remaining_path, current_count, best)
-        {
-            return Some(count + 1);
-        }
-    }
-    None
-}
-
 fn count_max_steps(
     start: (isize, isize),
     end: (isize, isize),
@@ -86,30 +126,20 @@ fn count_max_steps(
 fn count_max_steps_part2(
     start: (isize, isize),
     end: (isize, isize),
-    path: &HashMap<(isize, isize), TileType>,
-    current_count: &mut usize,
-    best: &mut usize,
+    path: &Nodes,
 ) -> Option<isize> {
     let mut counts: HashSet<isize> = HashSet::new();
-    if let Some(count) =
-        count_max_steps_direction_part2(start, end, (-1, 0), path, &mut current_count.clone(), best)
-    {
-        counts.insert(count);
-    }
-    if let Some(count) =
-        count_max_steps_direction_part2(start, end, (1, 0), path, &mut current_count.clone(), best)
-    {
-        counts.insert(count);
-    }
-    if let Some(count) =
-        count_max_steps_direction_part2(start, end, (0, -1), path, &mut current_count.clone(), best)
-    {
-        counts.insert(count);
-    }
-    if let Some(count) =
-        count_max_steps_direction_part2(start, end, (0, 1), path, &mut current_count.clone(), best)
-    {
-        counts.insert(count);
+    let next_junctions = path.get(&start).unwrap();
+    let mut remaining_path = path.clone();
+    remaining_path.remove(&start);
+    for j in next_junctions {
+        if j.0 == end {
+            counts.insert(j.1 as isize);
+        } else if remaining_path.contains_key(&j.0) {
+            if let Some(count) = count_max_steps_part2(j.0, end, &remaining_path) {
+                counts.insert(j.1 as isize + count);
+            }
+        }
     }
     counts.iter().max().copied()
 }
@@ -155,11 +185,16 @@ pub(crate) fn day23() {
             }
         })
     });
+
+    // Brute force part 1.
     println!("Day 23 Part 1: {:?}", count_max_steps(start, end, &path));
-    let mut current_count: usize = 0;
-    let mut best: usize = 6250;
+
+    // Brute forcing part 2 was less successful. Simplify the problem by reducing the path to a
+    // graph of junctions, and brute force those. Still slow, but good enough for me.
+    let mut junctions: Nodes = HashMap::new();
+    build_junction_graph(start, (start.0 + 1, start.1), &path, &mut junctions);
     println!(
         "Day 23 Part 2: {:?}",
-        count_max_steps_part2(start, end, &path, &mut current_count, &mut best).unwrap()
+        count_max_steps_part2(start, end, &junctions).unwrap()
     );
 }
